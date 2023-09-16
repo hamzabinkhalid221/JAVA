c56296049a1f04bad4e1535ea1c34085a0dfc181
hamzabinkhalid221 <142832021+hamzabinkhalid221@users.noreply.github.com>
2023-09-16 15:12:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+// for(a=1;a<=10;a++){
+//     document.write(a, "<br>");
+// }
+// var table = prompt("Enter the table");
+// var length = prompt("Enter the lenght");
+// for(var a=1;a<=length;a++){
+//     document.write(table + "* " +a + " = "+(a*table) +"<br>");
+// }
+
+// var fruit = ["apple","Bannana","Mango","Orange","Strabuery"];
+// for(var a=0;a<fruit.length;a++){
+//     document.write("Element of " + fruit[a] +a +"<br>");
+// }
+// document.write("COUNTING: <br>")
+// for(var a=1;a<=20;a++){
+//     document.write(a,",");
+// }
+
+// document.write("Reverse Counting:<br>");
+// for(var a=15;a>=1;a--){
+//     document.write(a,",");
+// }
+
+// document.write("Even:<br>");
+// for(var a=0;a<=20;a=a+2){
+//     document.write(a,",");
+// }
+// document.write("ODD:<br>");
+// for(var a=1;a<=19;a=a+2){
+//     document.write(a,",");
+// }
+
+// var i;
+// for (i = 0; i <= limit; i = i + 2) {
+
+//     console.log(i + "k");
+
+// }
+
+
+
+
+
+
+
+// var A = [24, 53, 78, 91, 12];
+// document.write("Array Items:",A +"<br>");
+
+
+// var largest = A[0]; 
+
+
+// for (var  i = 1; i < A.length; i++) {
+//   if (A[i] > largest) {
+//     largest = A[i];
+//   }
+// }
+
+
+// document.write("The largest number in the array is: " + largest);
+
+
+
+// var A = [24, 53, 78, 91, 12];
+// document.write("Array in item:",A +"<br>");
+
+
+// var smallest = A[0]; // Assume the first element is the smallest
+
+
+// for (var i = 1; i < A.length; i++) {
+//   if (A[i] < smallest) {
+//     smallest = A[i];
+//   }
+// }
+
+
+// document.write("The smallest number in the array is: " + smallest);
+
+
+// for (var i = 1; i <= 100; i++) {
+
+//   if (i % 5 === 0) {
+//     document.write(i,",");
+//   }
+// }
+
+
+// var a = ["Cake","Apple","Pie","Cookie","Chip","Patties"];
+// var input = prompt("What do you Want");
+// for(var i=0; i < a.length; i++){
+//   if(input == a[i]) {
+//     console.log("YES AVAIBLE");
+//   }
+// else{
+//   console.log("SORRY");
+// }
+// }
+
+// var matchFound = false;
+// var list = ["item1", "item2", "item3"]; 
+// var userInput = "input"; 
+
+// for (var i = 0; i < list.length; i++) {
+//   if (userInput === list[i]) {
+//     alert("Match found");
+//     matchFound = true;
+//     break;
+//   }
+// }
+
+// if (!matchFound) {
+//   alert("No match found");
+// }
+
+
+
