878d7fbcb5d68cd5bf0389ff8be3ded663cbb820
hamzabinkhalid221 <142832021+hamzabinkhalid221@users.noreply.github.com>
2023-09-22 22:58:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,207 @@
+// CHAPTER NO 21
+
+
+// // var allLower = userInput.toLowerCase();
+// var x = "YourStringHere"; 
+// x = x.toLowerCase();
+// console.log(x);
+// var y = "YourStringHerer";
+// y = y.toUpperCase();
+// console.log(y);
+// var originalString = "YourStringHere";
+// var lowerCaseString = originalString.toLowerCase();
+// var myString = "Hamza"; 
+// var upperCaseString = myString.toUpperCase();
+
+// alert(upperCaseString);
+// var cityName = "kaRacHi";
+// var capitalizedCityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+
+// console.log(capitalizedCityName);
+
+
+
+// CHAPTER NO: 22-25
+// var sameWords = "captain";
+// var slicedWord = sameWords.slice(1, 3); 
+
+// console.log(slicedWord); 
+
+// var myString = "Hello, world!";
+// var stringLength = myString.length;
+
+// console.log(stringLength);
+
+// var animal = "elephant";
+// var seg = animal.slice(1, 5); 
+
+// console.log(seg);
+
+// var myString = "Hello, world!";
+// var stringLength = myString.length; 
+
+// var slicedString = myString.slice(1, -3); 
+
+// console.log(stringLength); 
+// console.log(slicedString); 
+
+// var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// var indx=3;
+// var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// var index=16;
+
+// var text = "Let's go for a walk, and then let's go grab some coffee to go.";
+// var indx = text.lastIndexOf("go");
+
+// console.log(indx);
+
+// if (yourString.indexOf(indexNum) !== -1) {
+//     // Your code here if the segment exists
+//   }
+//   var myString = "abcde";
+// var characterAtIndex2 = myString.charAt(2);
+
+// console.log(characterAtIndex2); // Output: "c"
+
+// var text = "This is a sample text.";
+// var cha = text.charAt(3);
+
+// console.log(cha); // Output: "s"
+
+// var str = "Hello, world!";
+// var x = str.charAt(str.length - 1);
+
+// console.log(x); // Output: "!"
+
+// var input = "This is a sample input.";
+// var cha = input.charAt(3);
+
+// // console.log(cha); // Output: "s"
+
+// if (yourString.charAt(2) === 'yourCharacter') {
+//     // Your code here if the 3rd character is the particular character
+//   }
+
+// var charArray = [];
+
+// var reply = "no, I don't know. It's a no from me.";
+
+// for (var i = 0; i < reply.length; i++) {
+//   charArray.push(reply.charAt(i));
+// }
+
+// var revisedReply = charArray.join("");
+
+// revisedReply = revisedReply.replace("no", "yes");
+
+// console.log(revisedReply); 
+
+
+// var str = "I have 1 apple and 2 oranges.";
+// var newStr = str.replace("1", "one");
+
+// console.log(newStr);
+
+// var x = "banana";
+// var y = x.replace(/a/g, "z");
+
+// console.log(y); 
+
+
+// CHAPTER NO :27
+
+
+// var random = Math.random();
+
+// var min = 1;
+// var max = 50;
+// var scaledRandom = Math.floor(random * (max - min + 1)) + min;
+
+// console.log(scaledRandom);
+
+// var randomNumber = Math.random();
+// console.log(randomNumber);
+
+
+// var random = Math.random();
+
+// var outcome = random < 0.5 ? "Heads" : "Tails";
+
+// console.log("The coin landed on: " + outcome);
+
+// var random  = Math.floor(Math.random() *6 )+1;
+// console.log("You rolled " +random);
+
+
+
+// // CHAPTER NO: 28,29
+
+
+// var str=143;
+// var integer=Number(str);
+// console.log(integer);
+
+// var str=3.145214;
+// var float = Number(str);
+// console.log(float);
+
+// var str = "123";
+// var intValue = parseInt(str);
+
+// if (!isNaN(intValue)) {
+//   console.log("Conversion to integer successful:", intValue);
+// } else {
+//   console.log("Conversion to integer failed.");
+// }
+
+  
+
+// var number="123";
+// var sting=number.toString();
+// console.log(sting);
+
+// var str=3.14;
+// var integer  = parseInt(str);
+// console.log(integer);
+
+
+
+
+// Chapter 30
+
+// var str = "3.14";
+// var floatValue = parseFloat(str);
+// var integerValue = Math.floor(floatValue); 
+
+// console.log(integerValue); 
+
+
+// var num=3.14514212151;
+// var newNum = num.toFixed(4).toString();
+// console.log(newNum);
+// if (num.toFixed(2).toString().length > 4) {
+//     // Your code here if the string representation has more than 4 characters
+//   }
+  
+// var num=3.124178142;
+// var neNum = num.toFixed(2).toString();
+// alert(neNum);
+
+
+
+
+
+  
+
+
+
+
+  
+
+
+
+
+
+
