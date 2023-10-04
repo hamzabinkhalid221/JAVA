8cd19b8d19a57fcf3566545429cba9e98ad2dfd7
hamzabinkhalid221 <142832021+hamzabinkhalid221@users.noreply.github.com>
2023-10-04 11:39:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,13 @@
 
 
 
-function hi(num1,num2){
+// function hi(num1,num2){
 
-}
-var num1 = parseInt(prompt("Enter first Number"));
-var num2 = parseInt(prompt("Enter second Number"));
-console.log(num1 + num2);
-hi(num1,num2);
+// }
+// var num1 = parseInt(prompt("Enter first Number"));
+// var num2 = parseInt(prompt("Enter second Number"));
+// console.log(num1 + num2);
+// hi(num1,num2);
 
 
 
