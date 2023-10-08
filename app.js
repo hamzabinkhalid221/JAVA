5a8df0608bfe247a73aa8a7a8f3b8c47a8d725e5
hamzabinkhalid221 <142832021+hamzabinkhalid221@users.noreply.github.com>
2023-10-08 13:13:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// function login(){
+//     var name = document.getElementById("name");
+//     var email = document.getElementById("email");
+//     console.log(name.value);
+//     console.log(email.value);
+// }
+
+
+// function set() {
+//         var inp = document.getElementById("input");
+//         inp.value = "Hamza";
+//       }
+
+// function login(){
+
+//     var name = document.getElementById("question");
+//     var email = document.getElementById("response");
+//     console.log(question.value);
+//     console.log(response.value);
+// }
