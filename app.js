efb2cce1836b28e4b5f9d55e11807100d931e6ad
hamzabinkhalid221 <142832021+hamzabinkhalid221@users.noreply.github.com>
2023-10-20 05:05:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,35 @@
-var stopwatch = document.getElementById("stopwatch");
-var startBtn = document.getElementById("start-btn");
-var timeoutId = null;
-var ms = 0;
-var sec = 0;
-var min = 0;
- 
-
-function start(flag) {
-    if (flag) {
-        startBtn.disabled = true;
+function addTask(){
+    var taskinput = document.getElementById('task');
+    var taskText = taskinput.value;
+    taskinput.value = taskText;
+    if(taskText.trim() === ''){
+        return;
     }
- 
-    timeoutId = setTimeout(function() {
-        ms = parseInt(ms);
-        sec = parseInt(sec);
-        min = parseInt(min);
- 
-        ms++;
- 
-        if (ms == 100) {
-            sec = sec + 1;
-            ms = 0;
-        }
-        if (sec == 60) {
-            min = min + 1;
-            sec = 0;
-        }
-        if (ms < 10) {
-            ms = '0' + ms;
-        }
-        if (sec < 10) {
-            sec = '0' + sec;
-        }
-        if (min < 10) {
-            min = '0' + min;
-        }
- 
-        stopwatch.innerHTML = min + ':' + sec + ':' + ms;
- 
-        
-        start();
- 
-    }, 10); 
+
+var taskList = document.getElementById('taskList');
+var listItem = document.createElement('li');
+listItem.innerHTML = `
+${taskText}
+<button onclick = "EditTask(this)">Edit</button>
+<button onclick = "DeleteTask(this)">Delete</button>
+`;
+taskList.appendChild(listItem);
 }
-function pause() {
-    clearTimeout(timeoutId);
-    startBtn.disabled = false;
+
+function EditTask(button){
+    var listItem = button.parentNode;
+    var taskText  = listItem.firstChild;
+    var newTaskText = prompt('Edit Task',taskText.textContent);
+    if(newTaskText !== null){
+        taskText.textContent = newTaskText;
+    }
 }
- 
-function reset() {
-    ms = 0;
-    sec = 0;
-    min = 0;
-    clearTimeout(timeoutId);
-    stopwatch.innerHTML = '00:00:00';
-    startBtn.disabled = false;
+
+function DeleteTask(button){
+    var listItem = button.parentNode;
+    var taskList = document.getElementById('taskList');
+    taskList.removeChild(listItem);
 }
+
+
+
