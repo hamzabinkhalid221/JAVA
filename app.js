63a13bd7df0e2a3110d0d1d865d3d8ae97ab31fe
hamzabinkhalid221 <142832021+hamzabinkhalid221@users.noreply.github.com>
2023-10-27 11:18:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,83 @@
-function addTask(){
-    var taskinput = document.getElementById('task');
-    var taskText = taskinput.value;
-    taskinput.value = taskText;
-    if(taskText.trim() === ''){
-        return;
+var quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "javascript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+var quiz= document.getElementById('quiz')
+var answerEls = document.querySelectorAll('.answer')
+var questionEl = document.getElementById('question')
+var a_text = document.getElementById('a_text')
+var b_text = document.getElementById('b_text')
+var c_text = document.getElementById('c_text')
+var d_text = document.getElementById('d_text')
+var NextBtn = document.getElementById('Next')
+var currentQuiz = 0
+var score = 0
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    var currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+    var answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+NextBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
     }
-
-var taskList = document.getElementById('taskList');
-var listItem = document.createElement('li');
-listItem.innerHTML = `
-${taskText}
-<button onclick = "EditTask(this)">Edit</button>
-<button onclick = "DeleteTask(this)">Delete</button>
-`;
-taskList.appendChild(listItem);
-}
-
-function EditTask(button){
-    var listItem = button.parentNode;
-    var taskText  = listItem.firstChild;
-    var newTaskText = prompt('Edit Task',taskText.textContent);
-    if(newTaskText !== null){
-        taskText.textContent = newTaskText;
-    }
-}
-
-function DeleteTask(button){
-    var listItem = button.parentNode;
-    var taskList = document.getElementById('taskList');
-    taskList.removeChild(listItem);
-}
-function deleteAll(){
-    var taskinput = document.getElementById('taskList');
-    taskList.innerHTML = "";
-}
-
-
-
-
-
+})
